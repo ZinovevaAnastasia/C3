6bcbbedf01c1891948818009cafda3fe780c4a91
Nasty <zinovevaai@mail.ru>
2023-09-05 11:39:47 +0300

    DZ ex23

---
+++ Ex23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23: Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//кубов чисел от 1 до N.
+
+void Tablesquer ( int num)
+{
+int index = 1;
+ while (index<=num)
+ {
+    Console.WriteLine($"Куб {index} -> {index*index*index}");
+    index++;
+ }   
+}
+
+Console.WriteLine("Введите целое число");
+Console.Write("N = ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+Tablesquer(numb);
